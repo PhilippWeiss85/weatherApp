90880fd8a85998497068ff4d17c30794f9864082
Philipp <p.weiss85gmx.de>
2023-02-21 10:47:23 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import useSWR from "swr";
+import { useState } from "react";
+import { BarLoader } from "react-spinners";
+
+const baseUrl = "https://goweather.herokuapp.com/weather/";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [city, setCity] = useState("Curitiba");
+  const [fetchError, setFetchError] = useState(false);
+
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(baseUrl + city, fetcher);
+
+  const weather = data;
+
+  console.log("fetch", data);
+  console.log("city", city);
+
+  if (error)
+    return (
+      <>
+        <Header />
+        <main>
+          <section>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="city">Enter your City</label>
+              <input name="city" id="city" type="text" />
+              <button type="submit" id="sumbit">
+                search
+              </button>
+            </form>
+            <h2>An error occured. Please try again later</h2>
+          </section>
+        </main>
+        <Footer />
+      </>
+    );
+
+  if (isLoading)
+    return (
+      <div className="loadingscreen">
+        <BarLoader color="#36d7b7" height={10} width={300} />
+      </div>
+    );
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    console.log("forminput", data);
+    setCity(data.city);
+  }
+
+  // debounce function suchen für onChange event in form
+
+  if (data)
+    return (
+      <>
+        <Header />
+        <main>
+          <section>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="city">Enter your City</label>
+              <input name="city" id="city" type="text" />
+              <button type="submit" id="sumbit">
+                search
+              </button>
+            </form>
+            {fetchError !== true ? (
+              <h2>The Weather in {city}</h2>
+            ) : (
+              <h2>An error occured. Please try again later</h2>
+            )}
+
+            <div>
+              {/*           {currentWeather.description.includes("snow") === true ? (
+            <BsSnow />
+          ) : 
+          currentWeather.description.includes("sunny") === true ? (  <BsFillSunFill />) :
+          (
+            currentWeather.description.includes("sunny") === true ? (  <BsFillSunFill />) 
+          )}  */}
+              <div>{weather.description}</div>
+              <div>{weather.temperature} </div>
+              <div>{weather.wind} </div>
+            </div>
+
+            <h2>Forecast</h2>
+            {weather.forecast?.map((day) => {
+              return (
+                <div className="flexbox" key={day.day}>
+                  <p>{day.day}</p>
+                  <p>{day.temperature}</p>
+                  <p>{day.wind}</p>
+                </div>
+              );
+            })}
+          </section>
+        </main>
+        <Footer />
+      </>
+    );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import lottie from "lottie-web";
+import { useEffect, useRef } from "react";
+
+export default function Header() {
+  const container = useRef(null);
+
+  useEffect(() => {
+    const animation = lottie.loadAnimation({
+      container: container.current, // the dom element that will contain the animation
+      renderer: "svg",
+      loop: false,
+      autoplay: true,
+      animationData: require("../logo_small.json"), // the path to the animation json
+    });
+    return () => animation.destroy();
+  }, []);
+
+  return (
+    <section className="header">
+      <div className="container" ref={container}></div>
+      <h1>Your weather app</h1>
+    </section>
+  );
+}
