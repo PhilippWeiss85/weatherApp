36cf049585a97c23b04c3d1437fe6a0c5aeb911f
Philipp <p.weiss85gmx.de>
2023-02-21 14:53:17 +0100

    update ternary

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import cloudyIcon from "./icon_cloudy.png";
 import snowyIcon from "./icon_snow.png";
 import rainyIcon from "./icon_rain.png";
 import useSWR from "swr";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { BarLoader } from "react-spinners";
 
 const baseUrl = "https://goweather.herokuapp.com/weather/";
@@ -17,10 +17,67 @@ function App() {
   const [fetchError, setFetchError] = useState(false);
 
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
-  const { data, error, isLoading } = useSWR(baseUrl + city, fetcher);
+  const { data, error, isLoading } = useSWR(
+    city ? baseUrl + city : null,
+    fetcher
+  );
+
+  const tempEmoji = useCallback((description) => {
+    switch (description) {
+      case "Sunny":
+        return (
+          <div className="weather__icon">
+            <img
+              src={sunnyIcon}
+              alt="sunny"
+              style={{ width: "1rem", height: "1.2rem" }}
+            />
+            {description}
+          </div>
+        );
+      case "Partly cloudy":
+      case "mist":
+      case "drizzle":
+      case "Light drizzle":
+        return (
+          <div className="weather__icon">
+            <img
+              src={cloudyIcon}
+              alt="sunny"
+              style={{ width: "1.5rem", height: "1.2rem" }}
+            />
+            {description}
+          </div>
+        );
+      case "Light drizzle and raining":
+      case "Light rain":
+        return (
+          <div className="weather__icon">
+            <img
+              src={rainyIcon}
+              alt="sunny"
+              style={{ width: "1.5rem", height: "1.2rem" }}
+            />
+            {description}
+          </div>
+        );
+      case "Light snow":
+        return (
+          <div className="weather__icon">
+            <img
+              src={snowyIcon}
+              alt="sunny"
+              style={{ width: "1.5rem", height: "1.2rem" }}
+            />
+            {description}
+          </div>
+        );
+      default:
+    }
+  }, []);
 
   const weather = data;
-  const fetchMessage = data.message;
+  const fetchMessage = data?.message;
   console.log("fetchmessage", fetchMessage);
 
   console.log("fetch", data);
@@ -58,112 +115,73 @@ function App() {
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
     console.log("forminput", data);
-    setCity(data.city);
+    if (data.city === "undefined") {
+      setCity(null);
+    } else {
+      setCity(data.city);
+    }
   }
 
   // debounce function suchen für onChange event in form
 
-  let windspeedInt = parseInt(weather.wind);
+  let windspeedInt = parseInt(weather?.wind);
   console.log(parseInt(windspeedInt));
 
-  let temperatureInt = parseInt(weather.temperature);
+  let temperatureInt = parseInt(weather?.temperature);
   console.log(temperatureInt);
 
-  function tempEmoji(description) {
-    switch (description) {
-      case "Sunny":
-        return (
-          <div className="weather__icon">
-            <img
-              src={sunnyIcon}
-              alt="sunny"
-              style={{ width: "1rem", height: "1.2rem" }}
-            />
-            {description}
-          </div>
-        );
-      case "Partly cloudy":
-      case "mist":
-      case "drizzle":
-        return (
-          <div className="weather__icon">
-            <img
-              src={cloudyIcon}
-              alt="sunny"
-              style={{ width: "1.5rem", height: "1.2rem" }}
-            />
-            {description}
-          </div>
-        );
-      case "Light drizzle and raining":
-      case "Light rain":
-        return (
-          <div className="weather__icon">
-            <img
-              src={rainyIcon}
-              alt="sunny"
-              style={{ width: "1.5rem", height: "1.2rem" }}
-            />
-            {description}
-          </div>
-        );
-      case "Light snow":
-        return (
-          <div className="weather__icon">
-            <img
-              src={snowyIcon}
-              alt="sunny"
-              style={{ width: "1.5rem", height: "1.2rem" }}
-            />
-            {description}
-          </div>
-        );
-      default:
-    }
-  }
+  // hier ne if abfrage oder danach für das includes()
 
-  if (data)
-    return (
-      <>
-        <Header />
-        <main>
-          <section>
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="city">Enter your City</label>
-              <input name="city" id="city" type="text" />
-              <button type="submit" id="sumbit">
-                search
-              </button>
-            </form>
-            {fetchMessage === "NOT_FOUND" ? (
-              <h2>The API is unable to find your city</h2>
-            ) : fetchError !== true ? (
-              <h2>The Weather in {city.toUpperCase()}</h2>
-            ) : (
-              <h2>An error occured. Please try again later</h2>
-            )}
-            <div>
-              {tempEmoji(weather.description)}
+  return (
+    <>
+      <Header />
+      <main>
+        <section>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="city">Enter your City</label>
+            <input name="city" id="city" type="text" />
+            <button type="submit" id="sumbit">
+              search
+            </button>
+          </form>
+          {fetchMessage === "NOT_FOUND" ? (
+            <h2>Unable to find your city. Please try again</h2>
+          ) : fetchError !== true ? (
+            <h2>
+              {city !== undefined
+                ? `The Weather in ${
+                    city?.charAt(0).toUpperCase() + city?.slice(1)
+                  }`
+                : "Please enter a valid city"}
+            </h2>
+          ) : (
+            <h2>An error occured. Please try again later</h2>
+          )}
+          {data && (
+            <>
+              <div>
+                {tempEmoji(weather.description)}
+                <div>{weather.temperature} </div>
+                <div>{weather.wind} </div>
+              </div>
 
-              <div>{weather.temperature} </div>
-              <div>{weather.wind} </div>
-            </div>
-
-            <h2>Forecast</h2>
-            {weather.forecast?.map((day) => {
-              return (
-                <div className="flexbox" key={day.day}>
-                  <p>{day.day}</p>
-                  <p>{day.temperature}</p>
-                  <p>{day.wind}</p>
-                </div>
-              );
-            })}
-          </section>
-        </main>
-        <Footer />
-      </>
-    );
+              {fetchMessage === "NOT_FOUND" ? "" : <h2>Forecast</h2>}
+              {weather.forecast?.map((day) => {
+                return (
+                  <div className="flexbox" key={day.day}>
+                    <p>{day.day}</p>
+                    <p>{day.temperature}</p>
+                    <p>{day.wind}</p>
+                  </div>
+                );
+              })}
+            </>
+          )}
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
