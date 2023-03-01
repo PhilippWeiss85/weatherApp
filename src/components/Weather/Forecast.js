f57c0f519d2f37618ff1c4c04c0a3228c75424a6
Philipp <p.weiss85@gmx.de>
2023-03-01 19:33:01 +0100

    add motion to forecast

--- src/components/Weather/Forecast.js
+++ src/components/Weather/Forecast.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 function Forecast({ day, tomorrow, tomorrowPlusOne, tomorrowPlusTwo }) {
   return (
     <section>
       <div className="flexbox">
-        <div>
+        <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
           <aside className="forecast__containercontent__box__headline">
             {day.day === "1" ? (
               <div className="forecast-headline">
@@ -24,7 +25,7 @@ function Forecast({ day, tomorrow, tomorrowPlusOne, tomorrowPlusTwo }) {
             {day.temperature}
           </aside>
           <aside className="forecast__containercontent__box">{day.wind}</aside>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
